ded3e40f154ede35db999265a2d14610fd0106c9
yonghyun <dldydgus95426@gmail.com>
2024-11-05 21:21:08 +0900

    회원정보수정 하는 중!

--- human_s/src/main/java/com/human/hms/controller/MyPageController.java
+++ human_s/src/main/java/com/human/hms/controller/MyPageController.java
@@ -2,13 +2,20 @@ package com.human.hms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/mypage")
 public class MyPageController {
     
     // mypage.jsp로 이동
-    @GetMapping("/mypage")
+    @GetMapping("/mypage.do")
     public String showMyPage(){
         return "mypage/mypage";
+    }
+    // UserUpdate.jsp로 이동
+    @GetMapping("/update.do")
+    public String showUserUpdate(){
+        return "mypage/userUpdate";
     }
 }
